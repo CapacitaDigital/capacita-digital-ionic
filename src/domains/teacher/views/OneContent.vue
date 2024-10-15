710dd73a545c1b866fdb46fb58471bb5592761e8
root <root@CIGR-266237.ifpe.edu>
2024-10-15 10:03:11 -0300

    showOne

--- src/domains/teacher/views/OneContent.vue
+++ src/domains/teacher/views/OneContent.vue
@@ -9,7 +9,7 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const deserializedData = ref({});
+    const ddata = ref({});
 
     const deserializeData = (dataString: string) => {
       try {
@@ -21,18 +21,66 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      deserializedData.value = deserializeData(props.data);
-      console.log("Deserialized data:", deserializedData.value);
+      const params = new URLSearchParams(window.location.search);
+      const date = {
+        title: params.get('title'),
+        letra : params.get('letra'),
+        letrac: params.get('letrac')
+      }
+      ddata.value = deserializeData(props.data);
+      console.log("Deserialized data:", ddata.value);
+      console.log("DATE:", letras);
     });
 
-    return { deserializedData };
+    return { ddata };
   }
 });
 </script>
 
 <template>
-  <div>
-    <h1>Conteúdo</h1>
-    <p>Data : {{ deserializedData }}</p>
+  <div class="container">
+    <div class="form">
+      <img src="/public/img/abc.png" alt="abc">
+      <form action="" method="">
+        <input name="title" type="text" value="vogais">
+
+        <input class="letras" name="letra" type="text" value="A">
+        <input class="letras" name="letra" type="text" placeholder="letra">
+        <button type="submit">atualizar</button>
+      </form>
+    </div>
   </div>
 </template>
+
+<style scoped>
+
+.container{
+  display: flex;
+  flex-direction: column;
+}
+form{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+input{
+  border: 1px solid gray;
+}
+.letras{
+  text-align: center;
+  background-color: transparent;
+  height: 50px;
+  width: 50px;
+  border: 1px solid gray;
+  border-radius: 50%;
+}
+button{
+  margin: 0 auto;
+  height: 30px;
+  width: 100px;
+  border-radius: 5px;
+  color: white;
+  background-color: brown;
+  border: 1px solid gray;
+}
+</style>
